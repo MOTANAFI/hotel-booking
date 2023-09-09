22a6bdb7da4dcc8e9fb9302e5af925fd66529659
LONEWOLF143 <twaha2030@gmail.com>
2023-09-09 14:02:02 +0300

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStles";
+import  Button  from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-font-size: 30px;
-font-weight: 600;
-background-color: yellow;
-`
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 export default function App() {
   return (
-    <div>
-      <H1>hello hotelier</H1>
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+
+        <Heading as="h1">The wild Oasis</Heading>
+        <div>
+
+        <Heading as="h2">Checkin in and Out</Heading>
+        <Button>check me in</Button>
+        <Button>check me out</Button>
+        </div>
+        </Row>
+        <Row>
+        <Heading as="h3">Form</Heading>
+        <Row>
+        <Input type="number" placeholder="number of guests" />
+        <Input type="number" placeholder="number of guests" />
+        </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
