cb9aab1d5917d80109201f1e7ddeb417d64c430c
LONEWOLF143 <twaha2030@gmail.com>
2023-09-11 12:24:56 +0300

    setting up superbase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bmmtdheggenrvgswdwam.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtbXRkaGVnZ2VucnZnc3dkd2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQzMzE3NTgsImV4cCI6MjAwOTkwNzc1OH0.dRsHJSl3S8q9nnrSgxeyF-ek4tsmGCgKfSuC27gd300";
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
