374820c183c5a7c59948a0c88411202a2a642176
LONEWOLF143 <twaha2030@gmail.com>
2023-09-11 17:01:54 +0300

    setting up cabin table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -23,3 +23,20 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+
+function CabinTable() {
+  const x = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins,
+  })
+  console.log(x)
+  return (
+    <div>CabinTable</div>
+  )
+}
+
+export default CabinTable
