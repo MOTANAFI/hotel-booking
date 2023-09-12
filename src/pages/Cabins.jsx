c62d04ea8264012f9a4658cd1d68567f9c99894c
LONEWOLF143 <twaha2030@gmail.com>
2023-09-12 19:40:41 +0300

    implementing react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,13 @@
 
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm"
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [show, setShow] = useState(false)
   return (
     <>
     <Row type="horizontal">
@@ -13,6 +17,8 @@ function Cabins() {
       <Row>
         <CabinTable />
       </Row>
+        <Button onClick={() => setShow(!show)}>Add new Cabin</Button>
+        {show && <CreateCabinForm />}
     </>
   );
 }
