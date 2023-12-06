68d4cc01255892d1b5e59c2bbf8facda62f2b6f5
LONEWOLF143 <twaha2030@gmail.com>
2023-12-06 17:52:43 +0300

    fixed user loging laggin bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -4,16 +4,18 @@ import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-        queryClient.setQueriesData(['user'], user)
+      queryClient.setQueryData(["user"], user.user);
 
-      navigate("/dashboard");
+      navigate("/dashboard", {
+        replace: true,
+      });
     },
-    onError: (err) => {
+    onError: (error) => {
       toast.error("Provided email or password are incorrect");
     },
   });
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,24 +1,27 @@
-import supabase from "./supabase"
-export async function login({email, password}) {
-  const  { data, error } = await supabase.auth.signInWithPassword({
+import supabase from "./supabase";
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
-    password
-  })
+    password,
+  });
 
-  if(error) throw new Error(error.message)
-  
-  return data
+  if (error) throw new Error(error.message);
+
+  return data;
 }
 
 export async function getCurrentUser() {
-    const {data: session} = await supabase.auth.getSession()
-    if(!session.session) return null
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
 
-    const {data, error} = await supabase.auth.getUser()
+  const { data, error } = await supabase.auth.getUser();
 
-    console.log(data)
+  if (error) throw new Error(error.message);
 
-    if(error) throw new Error(error.message)
+  return data?.user;
+}
 
-    return data?.user
+export async function logout() {
+  const {error} = await supabase.auth.signOut()
+  if(error) throw new Error(error.message)
 }
