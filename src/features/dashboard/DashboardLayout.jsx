342afe7dd4fe49d3cd9f863d21ab62fe58de197c
LONEWOLF143 <twaha2030@gmail.com>
2023-12-14 22:00:32 +0300

    displaying statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import {useCabins} from "../cabins/useCabins"
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner"
+import Stats from "./Stats";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -15,11 +17,12 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const {bookings, isLoading: isLoading1} = useRecentBookings()
-  const {stays, confirmedStays, isLoading: isLoading2} = useRecentStays()
-  if(isLoading1 || isLoading2) return <Spinner />
+  const {stays, confirmedStays, isLoading: isLoading2, numDays} = useRecentStays()
+  const {cabins, isLoading: isLoading3} = useCabins()
+  if(isLoading1 || isLoading2 || isLoading3) return <Spinner />
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length} />
       <div>Today's activity</div>
       <div>Chart stay duration</div>
       <div>Chart sales</div>
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Stat from "./Stat";
+import {  HiOutlineBanknotes,
+    HiOutlineBriefcase,
+    HiOutlineCalendarDays,
+    HiOutlineChartBar, } from "react-icons/hi2";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  //1 number of bookings
+  const numBookings = bookings.length;
+
+  //2. total sales
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0)
+
+  //3. confirmed stays
+  const checkins = confirmedStays.length
+
+  //4. occupancy rate
+  const occupancy = confirmedStays.reduce((acc, cur) => acc + cur.numNights,0) / (numDays * cabinCount)
+  
+  // num checked in nights // all availabale nights(num days * num cabins)
+  return (
+    <>
+      <Stat
+        title="bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupancy * 100) + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
