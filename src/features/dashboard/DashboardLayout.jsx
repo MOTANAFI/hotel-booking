d34e678235d0daac552693bbc4d73403b2d6f23c
LONEWOLF143 <twaha2030@gmail.com>
2023-12-15 20:37:17 +0300

    display piechart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import {useCabins} from "../cabins/useCabins"
+import { useCabins } from "../cabins/useCabins";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
-import Spinner from "../../ui/Spinner"
+import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -13,22 +14,30 @@ const StyledDashboardLayout = styled.div`
   gap: 2.4rem;
 `;
 
-
-
-
 function DashboardLayout() {
-  const {bookings, isLoading: isLoading1} = useRecentBookings()
-  const {stays, confirmedStays, isLoading: isLoading2, numDays} = useRecentStays()
-  const {cabins, isLoading: isLoading3} = useCabins()
-  if(isLoading1 || isLoading2 || isLoading3) return <Spinner />
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
   return (
     <StyledDashboardLayout>
-      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length} />
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Today's activity</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <div>Chart stay duration</div>
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
-  )
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
